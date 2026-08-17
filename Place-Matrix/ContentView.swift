d51ead84cf69af20c8dec9c0a13de7df1a4819be
kenshi tsudome <t.kenshi.01@gmail.com>
2026-08-17 12:57:06 +0900

    Initial Commit

---
+++ Place-Matrix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Place-Matrix
+//
+//  Created by 津留健志 on 2026/08/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Place-Matrix/Place_MatrixApp.swift
@@ -0,0 +1,17 @@
+//
+//  Place_MatrixApp.swift
+//  Place-Matrix
+//
+//  Created by 津留健志 on 2026/08/17.
+//
+
+import SwiftUI
+
+@main
+struct Place_MatrixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
